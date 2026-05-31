f7fd8bc67cc2478b9f5e655dc40c6ad66c694b50
Arthur Wallace <114972252+WorldBuilder13@users.noreply.github.com>
2026-05-31 15:19:57 -0700

    Added scope

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -29,9 +29,10 @@ class Parser {
     program     ->  declaration*EOF;
     declaration ->  varDecl | statement;
     varDecl     ->  "var" IDENTIFIER ("=" expression)?";";
-    statement   ->  exprStmt | printStmt;
+    statement   ->  exprStmt | printStmt | block;
     exprStmt    ->  expression ";";
     printStmt   ->  "print" expression ";";
+    block       ->  "{" declaration* "}";
     expression  ->  equality | assignment;
     assignment  ->  IDENTIFIER "=" assignment | equality ;
     equality    ->  comparison (("!="|"==")comaprison)*;
@@ -89,6 +90,7 @@ class Parser {
 
     private Stmt statement(){
         if(match(PRINT)) return printStatement();
+        if(match(LEFT_BRACE)) return new Stmt.Block(block()); 
 
         return expressionStatement();
     }
@@ -115,6 +117,17 @@ class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block(){
+        List<Stmt> statements = new ArrayList<>();
+
+        while(!check(RIGHT_BRACE) && !isAtEnd()){
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
     }
 
     // assignment  ->  IDENTIFIER "=" assignment | equality ;
